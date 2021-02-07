5205d4865dc9f7ae64f69d00b9053f6280d1932b
Lionnarta <lionnarta.savirandy2000@gmail.com>
2021-02-07 22:04:16 +0700

    AngryBird project

---
+++ Assets/Scripts/OwlBird.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+    [SerializeField]
+    public float _explosionForce = 300;
+    public float _explosionRadius = 2;
+    public bool _hasExplode = false;
+
+    public GameObject ExplosionEffect;
+
+    public void Explode()
+    {
+        if (State == BirdState.Thrown && !_hasExplode)
+        {
+            _hasExplode = true;
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, _explosionRadius);
+            foreach(Collider2D col in colliders)
+            {
+                Vector2 dir = col.transform.position - transform.position;
+                col.GetComponent<Rigidbody2D>().AddForce(dir * _explosionForce);
+            }
+            RigidBody.AddForce(new Vector2(_explosionForce, 0), ForceMode2D.Impulse);
+
+            GameObject ExplosionEffectIns = Instantiate(ExplosionEffect, transform.position, Quaternion.identity);
+            Destroy(ExplosionEffectIns, 30);
+        }
+    }
+
+    public override void OnTap()
+    {
+        Explode();
+    }
+
+    public void OnCollisionEnter2D()
+    {
+        if (!_hasExplode)
+        {
+            Explode();
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _explosionRadius);
+    }
+}
---
+++ Assets/Scripts/TrailController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailController : MonoBehaviour
+{
+    public GameObject Trail;
+    public Bird TargetBird;
+
+    private List<GameObject> _trail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _trail = new List<GameObject>();
+    }
+
+    public void SetBird(Bird bird)
+    {
+        TargetBird = bird;
+
+        for(int i = 0; i < _trail.Count; i++)
+        {
+            Destroy(_trail[i].gameObject);
+        }
+
+        _trail.Clear();
+    }
+
+    public IEnumerator SpawnTrail()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        _trail.Add(Instantiate(Trail, TargetBird.transform.position, Quaternion.identity));
+        
+        if(TargetBird != null && TargetBird.State != Bird.BirdState.HitSomething)
+        {
+            StartCoroutine(SpawnTrail());
+        }
+    }
+}
